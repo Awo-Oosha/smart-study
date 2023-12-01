1fec7ab12a48f6834e35cb0e4b77dd98b34f19c8
Okediya Timileyin <OkediyaTimileyin@gmail.com>
2023-12-01 09:01:17 +0100

    feat: mobile navbar

--- src/styles/index.js
+++ src/styles/index.js
@@ -2,5 +2,9 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  padding: 0 53px;
+  padding: 0 12px;
+
+  @media (min-width: 992px) {
+    padding: 0 53px;
+  }
 `;
